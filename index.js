70a7a3991c47bfabd7d17e5a77636b3e931a17de
Rafi ullah <rafiullahmiani1509@gmail.com>
2025-09-01 23:15:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 function onSubmit() {
   const input = document.querySelector("#cityInput");
   const cityName = input.value;
-  console.log("City:", cityName);
+
+  if (!cityName) {
+    document.querySelector("#result").innerText = "Please enter a city name!";
+    return;
+  }
 
   axios
     .get(`https://p2pclouds.up.railway.app/v1/learn/weather?city=${cityName}`)
     .then((res) => {
       const feelsLike = res.data.current.feelslike_c;
-      console.log("Feels like:", feelsLike);
-
-      document.querySelector(
-        "#result"
-      ).innerText = `Feels like: ${feelsLike}°C`;
+      document.querySelector("#result").innerText = `Feels like: ${feelsLike}°C`;
     })
     .catch((error) => {
       console.error("Error:", error);
-      document.querySelector("#result").innerText = "City not found";
-      
-      });
+      document.querySelector("#result").innerText = "City not found ❌";
+    });
 }
